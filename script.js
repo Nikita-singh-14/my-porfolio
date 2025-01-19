c5e7dda96a8f1c8bf77db1ff319c152bd857b231
Nikita-singh-14 <nk2371174@gmail.com>
2025-01-19 08:34:16 +0530

    This is my fiirst commit

---
+++ script.js
@@ -0,0 +1,52 @@
+const hamburger= document.querySelector(".hamburger");
+const navBar=document.querySelector(".nav-bar");
+const boxes = document.querySelectorAll('.box');
+const project=document.querySelectorAll('.project');
+const photo = document.querySelector('.photo');
+
+
+
+hamburger.onclick=function(){
+  
+  navBar.classList.toggle("active");
+}
+
+
+
+
+boxes.forEach(box => {
+  box.addEventListener('mouseover', () => {
+    box.style.transform = 'scale(1.1)';
+    box.style.boxShadow = '0px 0px 10px rgba(0,0,0,0.5)';
+  });
+
+  box.addEventListener('mouseout', () => {
+    box.style.transform = 'scale(1)';
+    box.style.boxShadow = 'none';
+  });
+});
+
+
+
+project.forEach(project => {
+  project.addEventListener('mouseover', () => {
+    project.style.transform = 'scale(1.1)';
+    project.style.boxShadow = '0px 0px 10px rgba(0,0,0,0.5)';
+  });
+
+  project.addEventListener('mouseout', () => {
+    project.style.transform = 'scale(1)';
+    project.style.boxShadow = 'none';
+  });
+});
+
+
+photo.addEventListener('mouseover', () => {
+  photo.style.transform = 'scale(1.1)';
+  photo.style.boxShadow = '0px 0px 10px rgba(0,0,0,0.5)';
+});
+
+photo.addEventListener('mouseout', () => {
+  photo.style.transform = 'scale(1)';
+  photo.style.boxShadow = 'none';
+});
